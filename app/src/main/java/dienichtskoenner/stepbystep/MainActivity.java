ed0eff72ab8bb2a3fd790fe5c33230189c64eb5c
chris050910 <christian.lisik@gmx.net>
2017-08-28 21:59:43 +0200

    Fragments were added

--- app/src/main/java/dienichtskoenner/stepbystep/MainActivity.java
+++ app/src/main/java/dienichtskoenner/stepbystep/MainActivity.java
@@ -1,15 +1,32 @@
 package dienichtskoenner.stepbystep;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FragmentManager manager = getSupportFragmentManager();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Images were updated
 
+        initFragments();
+
+    }
+
+    private void initFragments() {
+
+        FragmentTop fragmentTop = new FragmentTop();
+        manager.beginTransaction()
+                .replace(R.id.layoutTop,fragmentTop,fragmentTop.getTag())
+                .commit();
+
+        FragmentBottom fragmentBottom=new FragmentBottom();
+        manager.beginTransaction()
+                .replace(R.id.layoutBottom,fragmentBottom,fragmentTop.getTag())
+                .commit();
     }
 }
