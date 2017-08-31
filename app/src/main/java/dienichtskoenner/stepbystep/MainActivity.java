855a9514871c64f988f4c36287c5f26a4c5cbe08
felixfoehnfrisur <30756683+felixfoehnfrisur@users.noreply.github.com>
2017-08-31 14:19:57 +0200

    Ansatz zur ActionBar

--- app/src/main/java/dienichtskoenner/stepbystep/MainActivity.java
+++ app/src/main/java/dienichtskoenner/stepbystep/MainActivity.java
@@ -3,6 +3,7 @@ package dienichtskoenner.stepbystep;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,9 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         initFragments();
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
     private void initFragments() {
