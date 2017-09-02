ecdbf18502df55667bebb1483d5bfa0545341ac6
chris050910 <christian.lisik@gmx.net>
2017-09-02 19:59:12 +0200

    Added Map in FragmentBottom

--- app/src/main/java/dienichtskoenner/stepbystep/FragmentBottom.java
+++ app/src/main/java/dienichtskoenner/stepbystep/FragmentBottom.java
@@ -1,18 +1,41 @@
 package dienichtskoenner.stepbystep;
 
 
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FragmentBottom extends Fragment {
+public class FragmentBottom extends Fragment implements OnMapReadyCallback {
 
+    private static final int REQUEST_LOCATION=1;
+    private static final float ZOOM_LOCATION=15;
+    private static final int TILT_LOCATION=0;
+
+    private SupportMapFragment mapFragment;
+    private GoogleMap mMap;
+    private Location location;
+    private LocationManager locationManager;
+
+    private boolean isLocationPermissionGranted;
+    private boolean isNetworkAvailable;
+    private boolean isOnResume;
+    private double latti,longi;
 
     public FragmentBottom() {
         // Required empty public constructor
@@ -26,4 +49,20 @@ public class FragmentBottom extends Fragment {
         return inflater.inflate(R.layout.fragment_bottom, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
 }
