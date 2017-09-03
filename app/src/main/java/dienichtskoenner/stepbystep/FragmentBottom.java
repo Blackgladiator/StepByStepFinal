59f82025c6457c41a8df1367637f085683fac7a4
chris050910 <christian.lisik@gmx.net>
2017-09-03 05:12:22 +0200

    AddedMap functions

--- app/src/main/java/dienichtskoenner/stepbystep/FragmentBottom.java
+++ app/src/main/java/dienichtskoenner/stepbystep/FragmentBottom.java
@@ -1,21 +1,32 @@
 package dienichtskoenner.stepbystep;
 
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 
 /**
@@ -33,7 +44,6 @@ public class FragmentBottom extends Fragment implements OnMapReadyCallback {
     private LocationManager locationManager;
 
     private boolean isLocationPermissionGranted;
-    private boolean isNetworkAvailable;
     private boolean isOnResume;
     private double latti,longi;
 
@@ -53,16 +63,122 @@ public class FragmentBottom extends Fragment implements OnMapReadyCallback {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        prepareMap();
         mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
     @Override
+    public void onResume() {
+        super.onResume();
+
+        if(isOnResume){
+            prepareMap();
+
+            mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+        }
+        isOnResume=true;
+    }
+
+    @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        updateLocationUI();
+    }
 
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    private void prepareMap() {
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)&&!isOnResume) {
+            buildAlertMessageNoGps();
+        }
+
+        else if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            getLocation();
+        }
+    }
+
+    public void getLocation() {
+        if (ActivityCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            isLocationPermissionGranted = false;
+            ActivityCompat.requestPermissions(getActivity(), new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        } else {
+            isLocationPermissionGranted=true;
+            location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+            if(location==null){
+                getLocation();
+            } else{
+                latti=location.getLatitude();
+                longi=location.getLongitude();
+            }
+        }
+    }
+
+    private void updateLocationUI() {
+
+        if (mMap == null) {
+            return;
+        }
+
+        if (ActivityCompat.checkSelfPermission(getActivity(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            isLocationPermissionGranted= true;
+        } else {
+            isLocationPermissionGranted=false;
+            ActivityCompat.requestPermissions(getActivity(), new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        }
+
+        if (isLocationPermissionGranted) {
+
+            mMap.setMyLocationEnabled(true);
+            mMap.getUiSettings().setMyLocationButtonEnabled(true);
+            CameraPosition cameraPosition = new CameraPosition.Builder()
+                    .target(new LatLng(latti,longi))
+                    .zoom(ZOOM_LOCATION)
+                    .tilt(TILT_LOCATION)
+                    .build();
+            CameraUpdate cameraUpdate= CameraUpdateFactory.newCameraPosition(cameraPosition);
+            mMap.animateCamera(cameraUpdate);
+        }else{
+            prepareMap();
+        }
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    private void buildAlertMessageNoGps() {
+
+        if(isNetworkAvailable()){
+            final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage(R.string.activate_gps)
+                    .setCancelable(false)
+                    .setPositiveButton(R.string.okay, new DialogInterface.OnClickListener() {
+                        public void onClick(final DialogInterface dialog, final int id) {
+
+                            startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+
+                        }
+                    })
+                    .setNegativeButton(R.string.disable, new DialogInterface.OnClickListener() {
+                        public void onClick(final DialogInterface dialog, final int id) {
+
+                            dialog.cancel();
+                        }
+                    });
+            final AlertDialog alert = builder.create();
+            alert.show();
+        }else {
+            getFragmentManager().beginTransaction().remove(FragmentBottom.this).commit();
+            getFragmentManager().beginTransaction().replace(R.id.layoutBottomNoInternet, new FragmentBottomNoInternet()).commit();
+        }
     }
 }
--- app/src/main/java/dienichtskoenner/stepbystep/FragmentBottomNoInternet.java
+++ app/src/main/java/dienichtskoenner/stepbystep/FragmentBottomNoInternet.java
@@ -2,17 +2,22 @@ package dienichtskoenner.stepbystep;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FragmentBottomNoInternet extends Fragment {
+public class FragmentBottomNoInternet extends Fragment implements View.OnClickListener{
 
+    ImageView refreshActivityIcon;
+    TextView refreshText;
 
     public FragmentBottomNoInternet() {
         // Required empty public constructor
@@ -26,4 +31,20 @@ public class FragmentBottomNoInternet extends Fragment {
         return inflater.inflate(R.layout.fragment_bottom_no_internet, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        refreshActivityIcon=(ImageView) getView().findViewById(R.id.refreshIcon);
+        refreshText=(TextView) getView().findViewById(R.id.refreshText);
+
+        refreshActivityIcon.setOnClickListener(this);
+        refreshText.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        getActivity().finish();
+        startActivity(getActivity().getIntent());
+    }
 }
