84384e8a8c645cbaa8c9eba2bcde47ec55227860
felixfoehnfrisur <30756683+felixfoehnfrisur@users.noreply.github.com>
2017-09-03 13:59:17 +0200

    Änderungen an der ActionBar - Hinzufügen des Logos

--- app/src/main/java/dienichtskoenner/stepbystep/MainActivity.java
+++ app/src/main/java/dienichtskoenner/stepbystep/MainActivity.java
@@ -14,7 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.logo);
         initFragments();
     }
 
