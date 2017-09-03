270f1b6e57dbab9acef9a39f15b26c28c12d076f
felixfoehnfrisur <30756683+felixfoehnfrisur@users.noreply.github.com>
2017-09-03 13:25:49 +0200

    Änderungen an der ActionBar

--- app/src/main/java/dienichtskoenner/stepbystep/MainActivity.java
+++ app/src/main/java/dienichtskoenner/stepbystep/MainActivity.java
@@ -14,7 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().setTitle("Step By Step");
         getSupportActionBar().setDisplayHomeAsUpEnabled(false);
         initFragments();
     }
