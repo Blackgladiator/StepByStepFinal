f53daaef37e25f5df83cc5eb789ec885e5988b9d
chris050910 <christian.lisik@gmx.net>
2017-09-09 15:22:37 +0200

    Added GeoFenceMainActivity.class

--- app/src/main/java/dienichtskoenner/stepbystep/MainActivity.java
+++ app/src/main/java/dienichtskoenner/stepbystep/MainActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import dienichtskoenner.stepbystep.geofence.GeoFenceMainActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     private FragmentManager manager = getSupportFragmentManager();
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.drawable.logo);
         initFragments();
@@ -28,19 +31,21 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
-        int id = item.getItemId();
-        if (id == android.R.id.home){
-            finish();
-        }
 
         switch (item.getItemId()){
             case R.id.action_bar_chart:
                 Intent intent = new Intent(MainActivity.this, Chart.class);
                 startActivity(intent);
                 break;
-        }
+            case R.id.action_bar_geofence:
+                Intent intentGeoFence=new Intent(MainActivity.this, GeoFenceMainActivity.class);
+                startActivity(intentGeoFence);break;
 
-        return super.onOptionsItemSelected(item);
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        
+        return false;
     }
 
     private void initFragments() {
