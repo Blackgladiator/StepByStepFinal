e3bb20d666965ca9d46076ebaa3aa260ab3784f2
felixfoehnfrisur <30756683+felixfoehnfrisur@users.noreply.github.com>
2017-09-20 19:22:37 +0200

    Neues Diagramm

--- app/src/main/java/dienichtskoenner/stepbystep/Chart.java
+++ app/src/main/java/dienichtskoenner/stepbystep/Chart.java
@@ -20,14 +20,21 @@ import java.util.Random;
 
 /**
  * Created by felixgross on 06.09.17.
+ * {https://github.com/PhilJay/MPAndroidChart}
  */
 
 public class Chart extends AppCompatActivity {
 
     BarChart barChart;
-    ArrayList<String> dates;
-    Random random;
-    ArrayList<BarEntry> barEntries;
+
+    int today;
+    int one_day_ago;
+    int two_days_ago;
+    int three_days_ago;
+    int four_days_ago;
+    int five_days_ago;
+    int six_days_ago;
+    int seven_days_ago;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,12 +45,11 @@ public class Chart extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.drawable.logo);
 
+        barChart = (BarChart) findViewById(R.id.bargraph);
 
-        barChart = (BarChart) findViewById(R.id.barchart);
-
-        createRandomBarGraph ("2016/05/05", "2016/06/01");
-
+        initBarChart();
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -67,68 +73,39 @@ public class Chart extends AppCompatActivity {
         return false;
     }
 
-    public void createRandomBarGraph (String Date1, String Date2){
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd");
+    private void initBarChart(){
 
-        try {
-            Date date1 = simpleDateFormat.parse(Date1);
-            Date date2 = simpleDateFormat.parse(Date2);
-
-            Calendar mDate1 = Calendar.getInstance();
-            Calendar mDate2 = Calendar.getInstance();
-
-            mDate1.clear();
-            mDate2.clear();
-
-            mDate1.setTime(date1);
-            mDate2.setTime(date2);
-
-            dates = new ArrayList<>();
-            dates = getList(mDate1, mDate2);
-
-            barEntries = new ArrayList<>();
-            float max = 0f;
-            float value = 0f;
-            random = new Random();
-            for (int j = 0; j < dates.size(); j++){
-                max = 100f;
-                value = random.nextFloat() * max;
-                barEntries.add(new BarEntry(value, j));
-            }
-
-        }catch(ParseException e){
-            e.printStackTrace();
-        }
-        BarDataSet barDataSet = new BarDataSet(barEntries, "Dates");
+        ArrayList<BarEntry> barEntries = new ArrayList<>();
+        barEntries.add(new BarEntry(44f, 0));
+        barEntries.add(new BarEntry(88f, 1));
+        barEntries.add(new BarEntry(66f, 2));
+        barEntries.add(new BarEntry(44f, 3));
+        barEntries.add(new BarEntry(12f, 4));
+        barEntries.add(new BarEntry(19f, 5));
+        barEntries.add(new BarEntry(91f, 6));
+        barEntries.add(new BarEntry(44f, 7));
+        BarDataSet barDataSet = new BarDataSet (barEntries, "Steps");
 
         ArrayList<IBarDataSet> dataSets = new ArrayList<IBarDataSet>();
 
         dataSets.add(barDataSet);
 
-        BarData barData = new BarData(dataSets);
-        barChart.setData(barData);
+        ArrayList<String> theDays = new ArrayList<>();
+        theDays.add("Today");
+        theDays.add("1 Day Ago");
+        theDays.add("2 Days Ago");
+        theDays.add("3 Days Ago");
+        theDays.add("4 Days Ago");
+        theDays.add("5 Days Ago");
+        theDays.add("6 Days Ago");
+        theDays.add("7 Days Ago");
+
+        BarData theData = new BarData(dataSets);
+        barChart.setData(theData);
+        barChart.setTouchEnabled(true);
+        barChart.setDragEnabled(true);
+        barChart.setScaleEnabled(true);
+
     }
-
-    public ArrayList<String> getList (Calendar startDate, Calendar endDate){
-        ArrayList<String> list = new ArrayList<>();
-        while (startDate.compareTo(endDate) <= 0){
-            list.add(getDate(startDate));
-            startDate.add(Calendar.DAY_OF_MONTH, 1);
-        }
-        return list;
-    }
-
-    public String getDate (Calendar cld){
-        String curDate = cld.get(Calendar.YEAR) + "/" + (cld.get(Calendar.MONTH) + 1) + "/" + cld.get(Calendar.DAY_OF_MONTH);
-
-        try {
-            Date date = new SimpleDateFormat("yyyy/MM/dd").parse(curDate);
-            curDate = new SimpleDateFormat("yyyy/MM/dd").format(date);
-        }catch(ParseException e){
-            e.printStackTrace();
-        }
-        return curDate;
-    }
-
 
 }
